28e220b0cc2c105c8a64840e6c8778510fa2a9f6
Leandro Ruel <proflrr@hotmail.com>
2022-06-08 22:58:57 -0300

    add balance module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BalanceModule } from '@src/balance/balance.module';
 
 @Module({
-  imports: [],
+  imports: [BalanceModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
