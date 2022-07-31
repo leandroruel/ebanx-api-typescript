53c8923e28db7769eacbddf021f628eb00cd7bf0
Leandro Ruel <proflrr@hotmail.com>
2022-07-31 16:47:50 -0300

    remove balance module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,12 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AccountsModule } from './accounts/accounts.module'
 import { AppService } from './app.service'
-import { BalanceModule } from '@src/balance/balance.module'
 import { PrismaModule } from '@src/prisma/prisma.module'
 import { CommonModule } from '@src/common/common.module'
 import { EventModule } from '@src/events/events.module'
 
 @Module({
-  imports: [
-    BalanceModule,
-    PrismaModule,
-    AccountsModule,
-    CommonModule,
-    EventModule
-  ],
+  imports: [PrismaModule, AccountsModule, CommonModule, EventModule],
   controllers: [AppController],
   providers: [AppService]
 })
