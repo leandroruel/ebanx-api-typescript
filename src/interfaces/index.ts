04050728c8eaf7985e89b440461949cc87f01873
Leandro Ruel <proflrr@hotmail.com>
2022-07-31 12:26:19 -0300

    add new interface and enum for events

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,3 +1,18 @@
-export interface AddBalanceResponse {
-  balance: number;
+/**
+ * Enum de tipos de evento que ocorrem na conta
+ */
+enum EventType {
+  TRANSFER = 'TRANSFER',
+  WITHDRAW = 'WITHDRAW',
+  DEPOSIT = 'DEPOSIT'
+}
+
+/**
+ * Representação de um evento de uma conta bancária
+ */
+export interface Event {
+  type: EventType
+  destination?: number
+  origin?: number
+  amount: number
 }
