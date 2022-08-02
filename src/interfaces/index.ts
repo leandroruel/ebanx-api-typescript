c46b9cba85c4443606d685bf640dd58ea98d4dbf
Leandro Ruel <proflrr@hotmail.com>
2022-08-02 17:54:54 -0300

    adiciona novas interfaces

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,3 +1,5 @@
+import { Accounts } from '@prisma/client'
+
 /**
  * Enum de tipos de evento que ocorrem na conta
  */
@@ -15,4 +17,26 @@ export interface Event {
   destination?: number
   origin?: number
   amount: number
+}
+
+/**
+ * Representação de uma resposta de um depósito em uma conta bancária.
+ */
+export interface DepositEvent {
+  destination: Accounts
+}
+
+/**
+ * Representação de uma resposta de um saque em uma conta bancária.
+ */
+export interface WithdrawEvent {
+  origin: Accounts
+}
+
+/**
+ * Representação de uma resposta de uma transferência em uma conta bancária.
+ */
+export interface TransferEvent {
+  origin: Accounts
+  destination: Accounts
 }
